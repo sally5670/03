d77dbdbd1bff9ca5eaac1a868651e3b248e0a6d2
sally5670 <144642113+sally5670@users.noreply.github.com>
2023-09-21 16:10:56 +0900

    Update main.c

--- main.c
+++ main.c
@@ -4,16 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(void) {
-	int input_int;
-	float input_float;
+	char c;
+	int i;
 	
-	printf("enter an integer : ");
-	scanf("%d",&input_int);
+	printf("input a number:");
+	scanf("%c", &c);// 만약 숫자5를 넣었어도 문자로 받아들여서 아스키코드5가 들어감.
 	
-	printf("enter a float : ");
-	scanf("%f",&input_float);
-	
-	printf("integer : %d, float %f\n",input_int,input_float);
+	i=c-'0';
+	printf("The input numbe ASCII is %i\n",c);
+	printf("The input number is %i \n", i); 
 	
 	system("PAUSE");
 	return 0;
